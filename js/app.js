95dbbc27eb8e82c39348343759e4ca86155c63c3
Amuskena <bjgamboa@gmail.com>
2022-02-22 13:42:01 +0100

    Desplegable

--- js/app.js
+++ js/app.js
@@ -7,19 +7,38 @@ const fetchData = async () => {
 		
 		const res = await fetch ("https://rickandmortyapi.com/api/character");
 		const data = await res.json();
+
+		let personajes = new Array();
+
 		//console.log(data);
-		document.write("LISTA DE PERSONAJES<br><br>");
+		//document.write("LISTA DE PERSONAJES<br><br>");
 		data.results.forEach(item => {
-		//console.log(item.name);
-		document.write(item.name + "<br>");
-		document.write('<img src="' + item.image +'"> <br>');
-		
-		});
+			//document.write(item.name + "<br>");
+			console.log(item.name + "<br>");
+			//document.write('<img src="' + item.image +'"> <br>');
+			let imagen = ('<img id="img' + item.id + '" src="' + item.image +'"> <br>');
+			//document.write(imagen);
+			personajes.push(item.name);
+
+
+			});
+		const select = document.querySelector('select');
+		for (let i in personajes){
+			select.options.add(new Option(personajes[i]));
+			//console.log(personajes[i]);
+		}
+
 
 		} catch (error) {
 		console.log(error);
 		} finally {
 	}
 };
+
+// function selectorPersonajes() {
+// 	const select = document.querySelector('select');
+// 	select.options.add(new Option('Text 1', 'Value1'))
+// }
+
 
 
