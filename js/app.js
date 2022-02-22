00ab167faeee942a43090cef1d30c648ff8fb786
Amuskena <bjgamboa@gmail.com>
2022-02-22 12:36:02 +0100

    Con imágenes

--- js/app.js
+++ js/app.js
@@ -4,17 +4,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const fetchData = async () => {
 	try {
+		
 		const res = await fetch ("https://rickandmortyapi.com/api/character");
 		const data = await res.json();
 		//console.log(data);
+		document.write("LISTA DE PERSONAJES<br><br>");
 		data.results.forEach(item => {
 		//console.log(item.name);
 		document.write(item.name + "<br>");
-	});
+		document.write('<img src="' + item.image +'"> <br>');
+		
+		});
 
-	} catch (error) {
+		} catch (error) {
 		console.log(error);
-	} finally {
+		} finally {
 	}
 };
+
 
